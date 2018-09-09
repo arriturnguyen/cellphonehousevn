f2d90f536b95fc25d4aaa4e244927185abdd5dfc
arriturnguyen <mrdatmoon@gmail.com>
2018-09-09 16:47:47 +0700

    refs dev #21 + category manage function

--- app/Category.php
+++ app/Category.php
@@ -6,8 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+	protected $fillable = [
+        'name', 'parent_id'
+    ];
+
     public function products()
     {
     	return $this->hasMany('App\Product');
+    }
+
+    public static function countChild($id)
+    {
+        return Category::where('parent_id', $id)->count();
+    }
+
+    public static function countParents()
+    {
+        return Category::where('parent_id', 0)->count();
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id', 'id');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
     }
 }
---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateCategoryRequest;
+use App\Http\Requests\EditCategoryRequest;
+use Mockery\Exception;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categoriesParent = Category::where('parent_id', 0)->paginate(10);
+        return view('admin.pages.categories.index', compact('categoriesParent'));
+    }
+
+    public function showChild(Category $category)
+    {
+        $categoriesChild = $category->children()->paginate(10);
+        $categoriesParent = $category;
+        return view('admin.pages.categories.showChild', compact('categoriesChild', 'categoriesParent'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categoriesParent = Category::where('parent_id', 0)->get();
+        return view('admin.pages.categories.create', compact('categoriesParent'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateCategoryRequest $request)
+    {
+        try {
+            $data = $request->all();
+            $category = Category::create($data);
+            if ($category) {
+                if ($request->parent_id == 0) {
+                    return redirect()->route('admin.categories.index')->with('message', __('category.admin.message.add'));
+                } else {
+                    return redirect()->route('admin.categories.showChild', $request->parent_id)->with('message', __('category.admin.message.add'));
+                }
+            } else {
+                return redirect()->route('admin.categories.create')->with('message', __('category.admin.message.add_fail'));
+            }
+        } catch (Exception $ex) {
+            return redirect()->route('admin.categories.create')->with('message', __('category.admin.message.add_fail'));
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Category  $Category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Category  $Category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        $categoriesParent = Category::where('parent_id', 0)->get();
+        return view('admin.pages.categories.edit', compact('categoriesParent', 'category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Category  $Category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(EditCategoryRequest $request, Category $category)
+    {
+        try {
+            $category->name = $request->name;
+            $category->parent_id = $request->parent_id != null ? $request->parent_id : 0;
+            $category->save();
+            if ($category) {
+                if ($request->parent_id == 0) {
+                    return redirect()->route('admin.categories.index')->with('message', __('category.admin.message.edit'));
+                } else {
+                    return redirect()->route('admin.categories.showChild', $request->parent_id)->with('message', __('category.admin.message.edit'));
+                }
+            } else {
+                return redirect()->route('admin.categories.edit')->with('message', __('category.admin.message.edit_fail'));
+            }
+        } catch (Exception $ex) {
+            return redirect()->route('admin.categories.edit')->with('message', __('category.admin.message.edit_fail'));
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Category  $Category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        try {
+            if ($category->parent_id != 0) {
+                $category->delete();
+                if (Category::countChild($category->parent_id)>0) {
+                    return redirect()->route('admin.categories.showChild', $category->parent_id)->with('message', __('category.admin.message.del'));
+                } else {
+                    return redirect()->route('admin.categories.index')->with('message', __('category.admin.message.del'));
+                }
+            } else {
+                if (Category::countChild($category->id)>0) {
+                    return redirect()->route('admin.categories.index')->with('alert', __('category.admin.message.del_no_permit'));
+                } else {
+                    $category->delete();
+                    return redirect()->route('admin.categories.index')->with('message', __('category.admin.message.del'));
+                }
+            }
+        } catch (Exception $ex) {
+            return redirect()->route('admin.categories.index')->with('message', __('category.admin.message.del_fail'));
+        }
+    }
+}
