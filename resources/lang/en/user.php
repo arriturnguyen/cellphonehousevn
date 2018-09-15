fcd4f78feea32ebe4c925d7ec7df49121a4a9697
arriturnguyen <mrdatmoon@gmail.com>
2018-09-15 23:18:21 +0700

    refs dev language

---
+++ resources/lang/en/user.php
@@ -0,0 +1,59 @@
+<?php
+
+	return [
+
+	    'admin' => [
+            'name' => 'Name',
+            'fullname' => 'Full Name',
+            'birthday' => 'Birthday',
+            'gender' => 'Gender',
+            'female' => 'Female',
+            'male' => 'Male',
+            'phone' => 'Phone',
+            'email' => 'Email',
+            'address' => 'Address',
+            'password' => 'Password',
+            'role' => 'Role',
+            'admin' => 'Admin',
+            'user' => 'User',
+            'active' => 'Active',
+            'inactive' => 'Inactive',
+            'actived' => 'Yes',
+            'deactived' => 'No',
+            'delete_success' => 'Delete user successfully',
+            'delete_fail' => 'Delete user failed!',
+            'create' => [
+                'title' => 'Create User',
+                'form_title' => 'Create Form',
+                'enter_username' => 'Enter your user name',
+                'enter_fullname' => 'Enter your full name',
+                'enter_birthday' => 'Enter your birthday',
+                'enter_phone' => 'Enter your phone number',
+                'enter_email' => 'Enter your email',
+                'enter_password' => 'Enter your password',
+                'create_user' => 'Create User',
+                'reset_user' => 'Reset',
+                'create_success' => 'Create user successfully',
+            ],
+            'edit' => [
+                'title' => 'Edit User',
+                'form_title' => 'Edit Form',
+                'update_user' => 'Update User',
+                'update_success' => 'Update user successfully',
+                'update_fail' => 'Update user failed!',
+            ],
+            'show' => [
+                'title' => 'Show User',
+                'form_title' => 'List Users',
+                'edit' => 'Edit',
+                'delete' => 'Delete',
+                'create_user' => 'Create User',
+                'role_admin' => 'Admin',
+                'role_shop_manager' => 'Shop Manager',
+                'role_user' => 'Customer',
+                'role_user' => 'User',
+                'delete_confirm' => 'Are you sure want to delete?'
+            ]
+        ]
+
+	];
